14c4915447993b463b3a5eb4b3888fc334850516
SimonElkhojHess <simo85j7@stud.kea.dk>
2024-12-02 11:54:54 +0100

    API call done
    API call to CapaOne database is done.

--- app/src/main/java/com/example/capaoneandroidhovedopgave/service/DeviceInfoService.java
+++ app/src/main/java/com/example/capaoneandroidhovedopgave/service/DeviceInfoService.java
@@ -1,13 +1,35 @@
 package com.example.capaoneandroidhovedopgave.service;
+import com.example.capaoneandroidhovedopgave.Portaluser;
+
+import java.io.IOException;
+
 import okhttp3.*;
 public class DeviceInfoService {
-    public void sendNewNameToDatabase(String jsonBody, String authToken) {
+    public static void sendNewNameToDatabase(String jsonBody, String authToken) {
         OkHttpClient client = new OkHttpClient();
 
+        Portaluser portaluserAuth = new Portaluser();
+        String portaluserAuthToken = portaluserAuth.getPortaluserToken();
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
         RequestBody body = RequestBody.create(jsonBody, JSON);
 
-        Request request = new Request.Builder().url("https://")
+        Request request = new Request.Builder().url("https://localhost:3300/api/organizations/1/android/enterprise/LC03g1yf5o/device/64365c56558de5ce65d85a82").header("X-Portaluser", portaluserAuthToken).post(body).build();
+
+        client.newCall(request).enqueue(new Callback() {
+           @Override
+           public void onFailure(Call call, IOException e) {
+               e.printStackTrace();
+           }
+
+           @Override
+           public void onResponse(Call call, Response response) throws IOException {
+               if (response.isSuccessful()) {
+                   response.body().string();
+               } else {
+                   System.err.println("Request failed with code: " + response.code());
+               }
+           }
+        });
     }
 }
